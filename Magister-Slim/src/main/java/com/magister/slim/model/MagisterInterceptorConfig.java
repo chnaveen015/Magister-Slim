78109e15ad10d3790cb099dad2c67d39d2885947
Sahithya4 <nuthisahithya44@gmail.com>
2020-01-08 10:29:19 +0530

    General Changes

--- Magister-Slim/src/main/java/com/magister/slim/model/MagisterInterceptorConfig.java
+++ Magister-Slim/src/main/java/com/magister/slim/model/MagisterInterceptorConfig.java
@@ -1,4 +1,5 @@
 package com.magister.slim.model;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

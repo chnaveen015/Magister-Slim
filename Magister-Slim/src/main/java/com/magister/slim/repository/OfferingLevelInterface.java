4e242ff50497e0517c5d60ca65911b922f36e012
naveenc <naveenc@INHYDNAVEENCH.COMAKEIT.COM>
2020-01-24 17:01:06 +0530

    Created Group

--- Magister-Slim/src/main/java/com/magister/slim/repository/OfferingLevelInterface.java
+++ Magister-Slim/src/main/java/com/magister/slim/repository/OfferingLevelInterface.java
@@ -8,4 +8,6 @@ import com.magister.slim.entity.OfferingLevel;
 public interface OfferingLevelInterface extends MongoRepository<OfferingLevel,String>{
 	@Query("{'offeringLevelName':?0}")
 	OfferingLevel getOfferingLevelByName(String offeringLevelName);
+	@Query("{'offeringLevelId':?0}")
+	OfferingLevel getOfferingLevel(String offeringLevelId);
 }
